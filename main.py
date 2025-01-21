80693281bf8699bbe86a3f40673429aa307fd18e
ibheelz <ibheelz@icloud.com>
2025-01-21 22:43:13 +0100

    Set pady to 1

--- main.py
+++ main.py
@@ -35,32 +35,32 @@ entry_width = 45  # Entry boxes width
 
 # Website Label and Entry
 website = tkinter.Label(window, text="Website:", font=(FONT_NAME, 15, "bold"), width=label_width, anchor="w", bg="#0b0926", fg="white")
-website.grid(column=0, row=1, padx=10, pady=5, sticky="w")
+website.grid(column=0, row=1, padx=10, pady=1, sticky="w")
 
 sitebox = tkinter.Entry(window, width=entry_width, bg="#251f6b", highlightthickness=0, bd=0)
-sitebox.grid(column=1, row=1, columnspan=2, pady=5)
+sitebox.grid(column=1, row=1, columnspan=2, pady=1)
 
-# Email Label and Entry
+# mail Label and Entry
 email = tkinter.Label(window, text="Email | Username:", font=(FONT_NAME, 15, "bold"), width=label_width, anchor="w", bg="#0b0926", fg="white")
-email.grid(column=0, row=2, padx=10, pady=5, sticky="w")
+email.grid(column=0, row=2, padx=10, pady=1, sticky="w")
 
 mailbox = tkinter.Entry(window, width=entry_width, bg="#251f6b", highlightthickness=0, bd=0)
-mailbox.grid(column=1, row=2, columnspan=2, pady=5)
+mailbox.grid(column=1, row=2, columnspan=2, pady=1)
 
 # Password Label and Entry
 password = tkinter.Label(window, text="Password:", font=(FONT_NAME, 15, "bold"), width=label_width, anchor="w", bg="#0b0926", fg="white")
-password.grid(column=0, row=3, padx=10, pady=5, sticky="w")
+password.grid(column=0, row=3, padx=10, pady=1, sticky="w")
 
 passbox = tkinter.Entry(window, width=25, bg="#251f6b", highlightthickness=0, bd=0)
 passbox.grid(column=1, row=3, pady=5)
 
 # Password Generation Button
 generate = tkinter.Button(window, text="Generate Password", font=(FONT_NAME, 13, "bold"), width=14, borderwidth=0)
-generate.grid(column=2, row=3, padx=10, pady=5)
+generate.grid(column=2, row=3, padx=10, pady=1)
 
 # Add Button
 add = tkinter.Button(window, text="Add", font=(FONT_NAME, 13, "bold"), width=42, borderwidth=0)
-add.grid(column=1, row=4, columnspan=2, pady=5)
+add.grid(column=1, row=4, columnspan=2, pady=1)
 
 # Running the main loop
 window.mainloop()
